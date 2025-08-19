ab4948733b004e13779dd53f6f30a0b4e4db8384
Joost Voskuil <joost.voskuil@navara.nl>
2025-08-19 13:38:44 +0200

    fix: update storage container resource to use storage account name instead of ID

--- hackathon/leaderboard/terraform/main.tf
+++ hackathon/leaderboard/terraform/main.tf
@@ -96,7 +96,7 @@ resource "azurerm_storage_account" "main" {
 
 resource "azurerm_storage_container" "main" {
   name                  = "challenges"
-  storage_account_id    = azurerm_storage_account.main.id
+  storage_account_name  = azurerm_storage_account.main.name
   container_access_type = "private"
 }
 
