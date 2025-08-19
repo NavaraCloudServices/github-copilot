b54cb2e02d6eb220cc5e9d52c33b285fa04c5b92
Joost Voskuil <joost.voskuil@navara.nl>
2025-08-19 13:05:49 +0200

    refactor: update resource group definition in Terraform configuration and clean up unused properties

--- hackathon/leaderboard/terraform/main.tf
+++ hackathon/leaderboard/terraform/main.tf
@@ -20,13 +20,8 @@ resource "random_password" "session_secret" {
   special = true
 }
 
-resource "azurerm_resource_group" "main" {
+data "azurerm_resource_group" "main" {
   name     = var.resource_group_name
-  location = var.location
-
-  lifecycle {
-    ignore_changes = [tags]
-  }
 }
 
 resource "azurerm_container_registry" "main" {
