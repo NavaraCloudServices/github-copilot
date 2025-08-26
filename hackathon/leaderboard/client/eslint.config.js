649176ab96b1de7edf78f8579131a7e586c354d0
Joost Voskuil <joost.voskuil@navara.nl>
2025-08-26 11:12:32 +0200

    chore: update client dependencies and ESLint configuration
    - Upgraded React and React DOM to version 19.1.1
    - Updated React Router DOM to version 7.8.2
    - Upgraded Vite and its React plugin to version 7.1.3 and 5.0.1 respectively
    - Updated framer-motion to version 12.23.12
    - Upgraded tailwind-merge to version 3.3.1
    - Updated ESLint and related plugins to version 9.34.0 and adjusted rules
    - Added new ESLint configuration file for improved linting
    - Documented migration steps and testing recommendations in DEPENDENCY_UPDATE_MIGRATION.md

---
+++ hackathon/leaderboard/client/eslint.config.js
@@ -0,0 +1,48 @@
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  {
+    ignores: ['dist'],
+  },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+        process: 'readonly',
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: '19.1' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/prop-types': 'off',
+      'no-unused-vars': ['error', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_' 
+      }],
+    },
+  },
+]
