d3047bee4dac32cbbf0ce8790bd1a7cca39bc44a
Joost Voskuil <joost.voskuil@navara.nl>
2025-08-26 08:45:59 +0200

    Fix grid layout in TeamView component to maintain consistent column count

--- hackathon/leaderboard/client/src/pages/TeamView.jsx
+++ hackathon/leaderboard/client/src/pages/TeamView.jsx
@@ -763,7 +763,7 @@ const TeamView = () => {
                 ) : (
                   <div className="space-y-4">
                     {/* Challenge Grid */}
-                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-4">
                       <AnimatePresence mode="popLayout">
                         {filteredChallenges.map((challenge, index) => (
                           <motion.div
